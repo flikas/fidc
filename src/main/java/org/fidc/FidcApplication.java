ef72a44e806b55cba2ad82572e268495373e1107
pengfei <pengfei@nantian.com.cn>
2018-12-31 00:01:13 +0800

    Initial commit.

---
+++ src/main/java/org/fidc/FidcApplication.java
@@ -0,0 +1,14 @@
+package org.fidc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FidcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FidcApplication.class, args);
+    }
+
+}
+
